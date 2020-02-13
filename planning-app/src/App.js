717fe2b173ab33ad41f6ff980c507d106967c61c
Sieugene <37626545+sieugene@users.noreply.github.com>
2020-02-13 22:12:21 +0300

    initial

---
+++ planning-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="App">
+            <h1>Planning App</h1>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
