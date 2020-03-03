1459616d8363b80f2f53d5626ccd5ba9fe76bbf6
Sieugene <37626545+sieugene@users.noreply.github.com>
2020-03-03 20:10:48 +0300

    Cloud Function Firestore Triggers

--- planning-app/src/Components/dashboard/Notification.js
+++ planning-app/src/Components/dashboard/Notification.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Notifications = (props) => {
     return(
-        <div>
-            <p>Notifications</p>
+        <div className='section'>
+            <div className="card blue-grey darken-1 project-summary">
+                <div className="card-content white-text">
+                    <span className="card-title">Notification</span>
+                    <p>Notification</p>
+                    <p>Notification</p>
+                    <p>Notification</p>
+                </div>
+            </div>
         </div>
     )
 }
